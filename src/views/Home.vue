7646400654eb5c745ac080290bafeee9526dcf27
cherry14 <765197818@qq.com>
2020-07-01 16:33:19 +0800

    1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,46 +4,11 @@
  * @Autor: xumeng
  * @Date: 2020-05-31 16:23:02
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2020-07-01 14:36:20
+ * @LastEditTime: 2020-07-01 16:27:35
 --> 
 <template>
-   <timeline :testList="testList" />
+   <div>
+     home
+   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import timeline from '@/components/timeline.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    timeline
-  },
-  data() {
-    return {
-      testList: [
-        {
-          text:'内容1rfrfrfrfrfrfrfrfrf分WFFFCEDFCEf',
-          time:'2020-02-02'
-        },
-        {
-          text:'内容2frrrrrrrr',
-          time:'2020-02-02'
-        },
-          {
-          text:'内容3frrrrrrgqerrrrrrrrrrr',
-          time:'2020-02-02'
-        },
-        {
-          text:'内容4',
-          time:'2020-02-02',
-        },
-        {
-          text:'内容5dfewwwwwwwwwg',
-          time:'2020-02-02'
-        },
-      ]
-    }
-  }
-}
-</script>
